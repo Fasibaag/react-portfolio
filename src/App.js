6d6ebeb6edc8ad650f54fe99d86708092be2a30b
Jacek Zarski <jacek.zarski@rutgers.edu>
2018-01-25 18:10:02 -0500

    routes and theme set up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import './App.css';
+import Routes from './components/Routes'
+import Reboot from 'material-ui/Reboot';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#6a90cf',
+      main: '#37639e',
+      dark: '#00396f',
+    }
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <MuiThemeProvider theme={theme}>
+          <Reboot/>
+          <Routes/>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Routes.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+import Grid from 'material-ui/Grid';
+
+const routes = [
+  {
+    path: '/',
+    navbar: () => <div> navbar </div>,
+    footer: () => <div> footer </div>
+  },
+  {
+    path: '/',
+    exact: true,
+    main: () => <div> landing </div>
+  },
+];
+
+class Routes extends Component {
+  render() {
+    return (
+      <Grid container alignItems={`center`} justify={`center`} spacing={24}>
+        <Grid item xs={12}>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              render={route.navbar}
+            />
+          ))}
+        </Grid>
+        <Grid item xs={12}>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              render={route.main}
+            />
+          ))}
+        </Grid>
+        <Grid item xs={12}>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              render={route.footer}
+            />
+          ))}
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default Routes;
