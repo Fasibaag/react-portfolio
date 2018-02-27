fcada19b5c330f23eb1e5b032cb19538eddc99d0
Jacek Zarski <jacek.zarski@rutgers.edu>
2018-02-27 17:32:44 -0500

    Added github link in projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
 import RecentWork from './RecentWork';
+import Button from 'material-ui/Button';
 
 class Projects extends Component {
   constructor(props) {
@@ -19,6 +20,16 @@ class Projects extends Component {
         </Grid>
         <Grid item>
           <RecentWork/>
+        </Grid>
+        <Grid item>
+          <Typography type="headline" align="center">
+            Check out more of my projects on Github!
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button href="https://github.com/zjacek12" color="primary" raised>
+            Github
+          </Button>
         </Grid>
       </Grid>
     );
